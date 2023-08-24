216f75e76e631c28ee29361d09bc635318df092c
yunse0708 <Yunse070708@gmail.com>
2023-08-24 23:17:41 +0900

    react 4강

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
 function App() {
 
   let post = '역삼 우동 맛집';
-  let [글제목, b] =  useState('여자 코트 추천');
-  let [제목, c] = useState('남성 운동화 추천');
-  let [와앙, d] = useState('남녀공용 나이키');
+  let [글제목, b] =  useState(['여자 코트 추천', '남성 운동화 추천', '남여공용 나이키']);
+  let [따봉, 따봉변경] = useState(0);
 
   return ( 
     <div className="App">
@@ -15,15 +13,17 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
       <div className="list">
-        <h4>{ 글제목 }</h4>
+        <h4>{ 글제목[0] } <span onClick={ () => { 따봉변경(따봉 + 1) } }>👍</span> { 따봉 }</h4>
         <p>2월 17일 발행</p>
-        </div>
+      </div>
+      
+
         <div className="list">
-          <h4>{ 제목 }</h4>
+          <h4>{ 글제목[1] }</h4>
           <p>3월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{ 와앙 }</h4>
+        <h4>{ 글제목[2] }</h4>
         <p>4월 24일 발행</p>
       </div>
       </div>
